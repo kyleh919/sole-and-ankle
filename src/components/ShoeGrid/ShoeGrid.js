6784d18e8a80b7290c5511d80b90dfdaade753f9
Kyle <kyle.hunter91@gmail.com>
2023-03-24 05:40:14 -0700

    E4A: Applies flex properties to ShoeGrid to create grid-like flex Adds 100% width to ShoeCard Image to take up entirety of container

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
     <Wrapper>
       {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
+        <ShoeCardWrapper>
+          <ShoeCard key={shoe.slug} {...shoe} />
+        </ShoeCardWrapper>
       ))}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px;
+`;
+
+const ShoeCardWrapper = styled.div`
+  min-width: 312px;
+  flex: 1;
+`;
 
 export default ShoeGrid;
